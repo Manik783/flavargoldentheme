c6698c064e610467293b3f2a202fed0d46051038
Manan Malik <mananm359@gmail.com>
2025-02-06 16:30:43 +0530

    Api calls added to restaurant_options and menu

--- backend/controllers/menu.controllers.js
+++ backend/controllers/menu.controllers.js
@@ -1,9 +1,11 @@
-const RestaurantModel = require('../models/restaurant.model');
+const RestaurantModel = require("../models/restaurant.model");
 
 //get restaurant info
 module.exports.getRestaurantInfo = async (req, res, next) => {
-    const { rest_id } = req.params;
-    const restaurant = await RestaurantModel.findOne({ rest_id: rest_id });
-    res.json({ restaurant });
-  };
-  
+  const { rest_id } = req.params;
+  const restaurant = await RestaurantModel.findOne(
+    { rest_id: rest_id },
+    "images email description name phone_number rating reviewers"
+  );
+  res.json({ restaurant });
+};
